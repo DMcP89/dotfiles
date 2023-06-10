aeec58f464fc87a2c2106f441156e8741d4bd970
DMcP89 <davemcpherson@wochstudios.com>
2023-06-10 13:55:48 -0400

    Moving code for installing ansible to make file

--- .local/bin/install-ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ ! $(which ansible) ]; then
-    sudo apt update 
-    sudo apt install software-properties-common -y
-    sudo add-apt-repository --yes --update ppa:ansible/ansible
-    sudo apt install ansible -y 
-fi
